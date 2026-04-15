cf95b79ebd69d3a5df02c24e2946c8bd72c8d2c7
varadjagtap09 <jagtapvarad18@gmail.com>
2026-04-15 19:11:23 +0530

    Add removeDuplicates function in Solution class
    Implement a function to remove duplicates from a sorted array.

---
+++ arrays_basics/Remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+class Solution {
+public:
+    int removeDuplicates(std ::vector<int>&arr ) {
+        int i=0;
+        for (int  j=1 ;j< arr.size() ; j++){
+            if (arr[i] !=arr[j]){
+                arr[i+1]=arr[j];
+                i++;
+            }
+        }
+        return i+1;
+        
+    }
+};
+
